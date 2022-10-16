5d57bf59fd98a9bb175ab342b8e0471245621330
Sheikhzain07 <sheikhzain459@gmail.com>
2022-10-17 05:04:38 +0530

    payment functionality updated

--- payment/scripts/checkout.js
+++ payment/scripts/checkout.js
@@ -1,3 +1,22 @@
+let netP=JSON.parse(localStorage.getItem("promocode"));
+// document.getElementById("netP").innerText=netP;
+let plan=JSON.parse(localStorage.getItem("plan"));
+if(netP==="MASAI500"){
+    if(plan==="PREMIUM"){
+        document.getElementById("netP").innerText=999;
+    } else if(plan==="SUPER"){
+        document.getElementById("netP").innerText=399;
+    } 
+} else {
+    if(plan==="PREMIUM"){
+        document.getElementById("netP").innerText=1499;
+    } else if(plan==="SUPER"){
+        document.getElementById("netP").innerText=899;
+    } else if(plan==="PREMIUM_MONTHLY"){
+        document.getElementById("netP").innerText=299;
+    }
+}
+
 document.querySelector("button").addEventListener("click",()=>{
 
     let name=document.getElementById("name").value;
